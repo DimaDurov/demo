12e60c0b2497748c6bf2786294380b7dd443558b
dimadurov <dima.stepok@yandex.ru>
2021-05-01 04:24:27 +0300

    new_v

---
+++ slamyTest/assets/js/openMethod.js
@@ -0,0 +1,168 @@
+$('#modal-href-1').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-1');
+});
+$('#modal-href-2').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-2');
+});
+$('#modal-href-3').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-3');
+});
+$('#modal-href-4').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-4');
+});
+$('#modal-href-5').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-5');
+});
+$('#modal-href-6').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-6');
+});
+$('#modal-href-7').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-7');
+});
+$('#modal-href-8').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-8');
+});
+$('#modal-href-9').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-9');
+});
+$('#modal-href-10').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-10');
+});
+$('#modal-href-11').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-11');
+});
+$('#modal-href-12').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-12');
+});
+$('#modal-href-13').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-13');
+});
+$('#modal-href-14').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-14');
+});
+$('#modal-href-15').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-15');
+});
+$('#modal-href-16').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-16');
+});
+$('#modal-href-17').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-17');
+});
+$('#modal-href-18').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-18');
+});
+$('#modal-href-19').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-19');
+});
+$('#modal-href-20').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-20');
+});
+$('#modal-href-21').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-21');
+});
+$('#modal-href-22').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-22');
+});
+$('#modal-href-23').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-23');
+});
+$('#modal-href-24').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-24');
+});
+$('#modal-href-25').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-25');
+});
+$('#modal-href-26').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-26');
+});
+$('#modal-href-27').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-27');
+});
+$('#modal-href-28').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-28');
+});
+$('#modal-href-29').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-29');
+});
+$('#modal-href-30').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-30');
+});
+$('#modal-href-31').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-31');
+});
+$('#modal-href-32').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-32');
+});
+$('#modal-href-33').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-33');
+});
+$('#modal-href-34').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-34');
+});
+$('#modal-href-35').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-35');
+});
+$('#modal-href-36').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-36');
+});
+$('#modal-href-37').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-37');
+});
+$('#modal-href-38').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-38');
+});
+$('#modal-href-39').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-39');
+});
+$('#modal-href-40').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-40');
+});
+$('#modal-href-41').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-41');
+});
+$('#modal-href-42').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-42');
+});
+$('#modal-href-43').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-43');
+});
+$('#modal-href-44').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-44');
+});
+$('#modal-href-45').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-45');
+});
+$('#modal-href-46').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-46');
+});
+$('#modal-href-47').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-47');
+});
+$('#modal-href-48').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-48');
+});
+$('#modal-href-49').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-49');
+});
+$('#modal-href-50').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-50');
+});
+$('#modal-href-51').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-51');
+});
+$('#modal-href-52').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-52');
+});
+$('#modal-href-53').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-53');
+});
+$('#modal-href-54').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-54');
+});
+$('#modal-href-55').on('click', function(){
+	$('#button-back-info').attr('data-bs-whatever','product-55');
+});
+
+
+
